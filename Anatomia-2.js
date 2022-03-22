3e05d2b8dac1335715cd54e7df179091d9500c71
João Vitor <joaovitorreal@outlook.com>
2022-03-22 13:49:10 -0300

    MateriaL

--- Anatomia-2.js
+++ Anatomia-2.js
@@ -1,1 +1,15 @@
 // Anonymous functions 
+(function (a, b, c) {    //exemplo de função anônima
+    return a + b + c
+})
+
+// Function expresssion --------------------------------------------------------------
+const soma = function (a, b) {     //função soma
+    return a + b 
+}
+
+const resultado = soma(8, 59)  //Váriavel recebendo a função soma
+console.log(resultado)
+
+const NoteSoma = soma
+console.log(NoteSoma(5, 5))  // Váriavel recendo a função soma e executando a partir do console.
