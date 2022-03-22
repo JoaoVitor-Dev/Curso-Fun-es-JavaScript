b10ba4f6c8ad7c94d6e4cb58e31240a76d7288b1
João Vitor <joaovitorreal@outlook.com>
2022-03-22 13:48:05 -0300

    Material

---
+++ Anatomia-3.js
@@ -0,0 +1,16 @@
+// Function expression
+const incremento1 = function(n) {
+    return n + 1
+}
+
+// Arrow Function | Obs: Funções Arrow sempre são anônimas  
+const incremento2 = (n) => {
+    return n + 1
+}
+
+const incremento3 = (n) => {
+    return n + 1
+}
+
+console.log(incremento1(1));
+console.log(incremento2(5));
---
+++ Anatomia-4.js
@@ -0,0 +1,4 @@
+// Anonymous function
+(function (a, b, c) {
+    console.log(`Resultado: ${a + b + c}`)     //exemplo de função anônima imediatamente invocada
+}) (1, 2, 3) //atribuição de valores 
