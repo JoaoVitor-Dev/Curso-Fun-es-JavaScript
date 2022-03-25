b0034c362e3dfb3e4985e54896707c0e092432f1
João Vitor - Web Developer <79536488+JoaoVitor-Dev@users.noreply.github.com>
2022-03-24 23:34:36 -0300

    Programação Funcional

---
+++ Programação Funcional/First-Class Function.js
@@ -0,0 +1,19 @@
+
+
+const add = function(a, b) {  //function expression
+    return a + b
+}
+const subtrack = function(a, b) {  //function expression
+    return a - b
+}
+
+const multiply = (x, y) => x * y // arrow function
+
+const divide = (x, y) => x / y  // arrow function
+
+
+console.log(add(10, 20));
+console.log(subtrack(10, 20));
+console.log(multiply(10, 20));
+console.log(divide(10, 20));
+
